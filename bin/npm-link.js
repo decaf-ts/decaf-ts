8674bd4d276d0c90e034cbe45a8d82a229c6e988
Tiago <venceslau.tiago@gmail.com>
2024-10-01 18:25:58 +0100

    refs #1 - update

--- bin/npm-link.js
+++ bin/npm-link.js
@@ -8,82 +8,41 @@ operation = operation ? operation.substring(2, operation.length) : "link"
 
 if (operation === "link"){
     libs.forEach(l => {
-        const iterations = l.includes("control-panel") ? [`${l}/frontend`, `${l}/backend`] : [l]
-        iterations.forEach(l => {
-            const pkg = require(path.join(process.cwd(), l, "package.json"))
-            const dependencies = [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.devDependencies || {})].filter(d => d.startsWith("@aeon/"));
-            dependencies.forEach(d => {
-                try {
+        const pkg = require(path.join(process.cwd(), l, "package.json"))
+        const dependencies = [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.devDependencies || {}), ...Object.keys(pkg.peerDependencies || {})].filter(d => d.startsWith("@decaf-ts/"));
+        dependencies.forEach(d => {
+            try {
 
-                    let pathToRemove = `node_modules/${d}`
-                    let linkRel = `../../../${l.includes("control-panel") ? "../" : ""}`
-                    let name = d.split("/")[1]
-                    let linkPath = name === "web-components" ? `${name}/dist/aeon-web-components` : name;
-                    let linkName = name;
-                    let cwd = path.join(process.cwd(), l, "node_modules/@aeon")
+                let pathToRemove = `node_modules/${d}/lib`;
+                let linkRel = `../../../`;
+                let name = d.split("/")[1]
+                let linkPath = name;
+                let linkName = name;
+                let cwd = path.join(process.cwd(), l, "node_modules/@decaf-ts")
 
-                    if (l.includes("control-panel/backend")){
-                        let p;
-                        if (d.includes("fabric-integration")){
-                            p = path.join(l, pathToRemove, "build")
-                            try {
-                                fs.statSync(p);
-                            } catch (e) {
-                                fs.mkdirSync(p)
-                            }
-                            pathToRemove = `node_modules/${d}/build`;
-                            linkName = "build";
-                        } else {
-                            p = path.join(l, pathToRemove, "lib")
-                            try {
-                                fs.statSync(p);
-                            } catch (e) {
-                                fs.mkdirSync(p)
-                            }
-                            pathToRemove = `node_modules/${d}/lib`;
-                            linkName = "lib";
-                        }
-                        linkRel = path.join(linkRel, "../")
-                        name = d.split("/")[1]
-                        linkPath = name + "/" + linkName;
-                        cwd = path.join(process.cwd(), l, "node_modules/@aeon", name)
-                    }
+                console.log(`${operation}ing ${d} as a dependency of ${l}`)
+                execSync(`rm -rf ${pathToRemove}`, {
+                    cwd: path.join(process.cwd(), l),
+                    env: process.env
+                })
+                execSync(`ln -s ${linkRel}${linkPath}/lib ${linkName}/lib`, {
+                    cwd: cwd,
+                    env: process.env
+                })
+            } catch (e){
+                process.exit(1)
+            }
 
-                    console.log(`${operation}ing ${d} as a dependency of ${l}`)
-                    execSync(`rm -rf ${pathToRemove}`, {
-                        cwd: path.join(process.cwd(), l),
-                        env: process.env
-                    })
-                    execSync(`ln -s ${linkRel}${linkPath} ${linkName}`, {
-                        cwd: cwd,
-                        env: process.env
-                    })
-                } catch (e){
-                    process.exit(1)
-                }
-
-            })
         })
     })
 } else if (operation === "unlink"){
     libs.forEach(l => {
-        const iterations = l.includes("control-panel") ? [`${l}/frontend`, `${l}/backend`] : [l]
-        iterations.forEach(l => {
-            const pkg = require(path.join(process.cwd(), l, "package.json"))
-            const dependencies = [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.devDependencies || {})].filter(d => d.startsWith("@aeon/"));
-            dependencies.forEach(d => {
-                console.log(`${operation}ing ${d} as a dependency of ${l}`)
-                try {
-                    execSync(`rm -rf  node_modules/@aeon/${d}`, {
-                        cwd: path.join(process.cwd(), l),
-                        env: process.env
-                    })
-                } catch (e){
-                    process.exit(1)
-                }
-            })
+        const pkg = require(path.join(process.cwd(), l, "package.json"))
+        const dependencies = [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.devDependencies || {}), ...Object.keys(pkg.peerDependencies || {})].filter(d => d.startsWith("@decaf-ts/"));
+        dependencies.forEach(d => {
+            console.log(`${operation}ing ${d} as a dependency of ${l}`)
             try {
-                execSync(`npm install`, {
+                execSync(`rm -rf  node_modules/@decaf-ts/${d}`, {
                     cwd: path.join(process.cwd(), l),
                     env: process.env
                 })
@@ -91,7 +50,16 @@ if (operation === "link"){
                 process.exit(1)
             }
         })
+        try {
+            execSync(`npm run do-install`, {
+                cwd: path.join(process.cwd(), l),
+                env: process.env
+            })
+        } catch (e){
+            process.exit(1)
+        }
     })
+
 } else {
     libs.forEach(l => {
         console.log(`${operation}ing ${l}`)
