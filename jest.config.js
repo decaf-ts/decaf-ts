4c608a6e7395b41da4e325b39bd05850e2d879e2
Tiago <venceslau.tiago@gmail.com>
2024-10-10 19:57:33 +0100

    refs #1 - update

--- jest.config.js
+++ jest.config.js
@@ -16,7 +16,7 @@ const jestConfigs = modules.map(m => fs.readdirSync(path.join(process.cwd(), m))
 module.exports = {
   projects: jestConfigs,
 
-  displayName: 'Decaf Multi Module Typescript template',
+  displayName: 'Decaf Workspace',
   // testPathIgnorePatterns: ['/node_modules/'],
   collectCoverageFrom: [
     '**/src/**/*.[jt]s',
@@ -25,14 +25,14 @@ module.exports = {
     '!**/src/**/*.e2e.[jt]s',
     '!**/tests/**/*.test.[jt]s',
   ],
-  coverageThreshold: {
-      global: {
-          statements: 90,
-          branches: 75,
-          functions: 85,
-          lines: 90,
-      },
-  },
+  // coverageThreshold: {
+  //     global: {
+  //         statements: 90,
+  //         branches: 75,
+  //         functions: 85,
+  //         lines: 90,
+  //     },
+  // },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   coverageDirectory: "./workdocs/coverage",
   reporters: [
@@ -40,36 +40,3 @@ module.exports = {
     ["jest-junit", {outputDirectory: './workdocs/coverage', outputName: "junit-report.xml"}]
   ]
 }
-
-
-module.exports = {
-  verbose: true,
-  transform: {'^.+\\.ts?$': 'ts-jest'},
-  testEnvironment: 'node',
-  testRegex: '/tests/.*\\.(test|spec|integration)\\.(ts|tsx)$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  collectCoverage: true,
-  coverageDirectory: "./workdocs/coverage",
-  collectCoverageFrom: ['src/**/*.{ts,jsx}'],
-  coveragePathIgnorePatterns: [
-    "*cli.ts"
-  ],
-  coverageThreshold: {
-    global: {
-      branches: 70,
-      functions: 100,
-      lines: 80,
-      statements: 90
-    }
-  },
-  coverageReporters: [
-    "json-summary",
-    "text-summary",
-    "text",
-    "html"
-  ],
-  reporters: [
-    "default",
-    ["jest-junit", {outputDirectory: './workdocs/coverage', outputName: "junit-report.xml"}]
-  ]
-};
