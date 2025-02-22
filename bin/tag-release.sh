d2f2ced0321ac41348356897599c297424c5c22f
Tiago <venceslau.tiago@gmail.com>
2025-02-22 14:59:30 +0000

    DECAF-109 release mechanism update
    Signed-off-by: Tiago <venceslau.tiago@gmail.com>

--- bin/tag-release.sh
+++ bin/tag-release.sh
@@ -70,13 +70,12 @@ fi
 echo "publishing all under version update $TAG with message $MESSAGE"
 
 npm run run-all -- "npm run update-dependencies --if-present && npm run release -- $TAG $MESSAGE"
+npm version "$TAG" -m "$MESSAGE"
 
 if [[ $(git status --porcelain) ]]; then
   git add .
   git commit -m "after release preparation"
 fi
-
-npm version "$TAG" -m "$MESSAGE"
 
 git push --follow-tags
 
